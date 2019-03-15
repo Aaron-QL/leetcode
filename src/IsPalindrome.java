a8c6270223968dd8c017c01c1ff306a05a0e222d
秦龙 <qinlong@akaqin.me>
2019-03-15 09:42:45 +0800

    leetcode

---
+++ src/IsPalindrome.java
@@ -0,0 +1,15 @@
+public class IsPalindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int hold = x, reverse = 0;
+        while (x > 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+
+        return hold == reverse;
+    }
+}
