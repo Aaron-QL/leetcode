bcaec8770a4758137d3a01ced363e847203f6ac3
akaQin <qinlong@akaqin.me>
2019-04-02 20:29:17 +0800

    RDFSL

---
+++ java_src/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,22 @@
+public class RemoveDuplicatesFromSortedList {
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
