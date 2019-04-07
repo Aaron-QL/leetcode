d9126c947665bcb43b91305d222da6a0ef6a5e12
akaQin <qinlong@akaqin.me>
2019-04-07 12:17:32 +0800

    SymmetrricTree

---
+++ java_src/SymmetricTree.java
@@ -0,0 +1,56 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SymmetricTree {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode x, TreeNode y) {
+        if (x == null && y == null) {
+            return true;
+        }
+
+        if (x == null || y == null) {
+            return false;
+        }
+
+        return x.val == y.val && check(x.left, y.right) && check(x.right, y.left);
+    }
+
+    public boolean isSymmetric2(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+}
