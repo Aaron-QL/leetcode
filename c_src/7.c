05128fe430e16b5dd62c3de7cb7cb2c14595539e
akaQin <qinlong@akaqin.me>
2019-12-08 20:59:48 +0800

    7

---
+++ c_src/7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int reverse(int x){
+    long val = 0;
+    do {
+        val = val * 10 + x % 10;
+        if (val < INT_MIN / 10 || val > INT_MAX / 10) {
+            return 0;
+        }
+    } while (x /= 10);
+    return (val > INT_MAX || val < INT_MIN) ? 0 : val;
+}
+
+int main() {
+    printf("%d\n", reverse(-1233213213121123));
+}
