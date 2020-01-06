cbf6892934cf3e20a1204f01b62e85df5222cce9
akaQin <qinlong@akaqin.me>
2020-01-06 11:47:26 +0800

    getBatchModuleCount

--- c_src/14.c
+++ c_src/14.c
@@ -1,4 +1,26 @@
+//Approach 1: Horizontal scanning
+char * longestCommonPrefix(char ** strs, int strsSize){
+    if (strsSize == 0) {
+        return "";
+    }
+    char *prefix = malloc((strlen(strs[0]) + 1) * sizeof(char));
+    strcpy(prefix, strs[0]);
+    for (int i = 1; i < strsSize; i++) {
+        compare(prefix, strs[i]);
+    }
+    return prefix;
+}
 
+void compare(char *dest, char *src)
+{
+    int i;
+    for (i = 0; (dest[i] == src[i]) && dest[i] != '\0'; i++) {
+
+    }
+    dest[i] = '\0';
+}
+
+//Approach 2: Vertical scanning
 char * longestCommonPrefix(char ** strs, int strsSize){
     char *first = strs[0];
     char item;
