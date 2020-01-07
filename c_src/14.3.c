46475bd7364c7fcfe328c4193a2e947a4418adc8
akaQin <qinlong@akaqin.me>
2020-01-07 17:46:32 +0800

    bug fixed

--- c_src/14.3.c
+++ c_src/14.3.c
@@ -23,18 +23,14 @@ char *divideAndConquer(char **strs, int left, int right)
 
 char *compare(char *s1, char *s2)
 {
-    int len = strlen(s1) < strlen(s2) ? strlen(s1) : strlen(s2);
-    char *p = malloc((len + 1) * sizeof(char));
-    char *pp = p;
+    char *p = s1;
     while (*s1 == *s2) {
-        if (*s1 != '\0') {
-            *pp++ = *s1;
-        } else {
+        if (*s1 == '\0') {
             break;
         }
         s1++;
         s2++;
     }
-    *pp = '\0';
+    *s1 = '\0';
     return p;
 }
