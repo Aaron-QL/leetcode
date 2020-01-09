2f061c6e983a76e016f1dafffe8db001eb09ba06
akaQin <qinlong@akaqin.me>
2020-01-09 17:51:52 +0800

    14.4

---
+++ include/my.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+#include <limits.h>
+#include <sys/param.h>
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "../include/my.h"
+
+int main(int argc, char **argv)
+{
+    printf("%s\n", longestCommonPrefix(argv, argc));
+    return 0;
+}
