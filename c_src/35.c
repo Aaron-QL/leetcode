d390a3d9abfeeb441b46349f7926fb8520dc78f2
akaQin <qinlong@akaqin.me>
2020-01-19 14:55:44 +0800

    35

---
+++ c_src/35.c
@@ -0,0 +1,14 @@
+
+
+int searchInsert(int* nums, int numsSize, int target){
+    int i;
+    for (i = 0; i < numsSize; i++) {
+        if (nums[i] == target) {
+            return i;
+        } else if (nums[i] > target) {
+            return i;
+        }
+    }
+    return numsSize;
+}
+
