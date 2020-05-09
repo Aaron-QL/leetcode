44709abf030cd97c32ce0d159d7fc8c8e9a223f8
akaQin <qinlong@akaqin.me>
2020-05-09 10:40:15 +0800

    reformat

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # leetcode
+
+每天一道leetcode，包含c、go、java三种实现
---
+++ java_src/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.lenth < 1) {
+            return "";
+        }
+        for (int i = 0; i < s.l)
+    }
+
+    private int t(String s, int lo, int hi) {
+
+    }
+}
